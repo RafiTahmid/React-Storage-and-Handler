e29afd165c0c1c2eff4ddde0a11edeedaf0d86c9
RafiTahmid <rafi.tahmid019@gmail.com>
2022-09-25 20:00:06 +0600

    Event handler added

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,12 +1,18 @@
 import React from 'react';
-
+import './Cosmetic.css'
 const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic
+
+    const addToCart = () => {
+        console.log('Item Added', id)
+    }
     return (
-        <div>
+        <div className='product'>
             <h2>Buy this:{name}</h2>
             <p>Only for: ${price}</p>
             <p><small>It has id: {id}</small></p>
+            
+            <button onClick={() => addToCart(id)}>Add To Cart</button>
         </div>
     );
 };
