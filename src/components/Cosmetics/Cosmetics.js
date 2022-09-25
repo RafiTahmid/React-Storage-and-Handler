6bb4f60937a1b414c7bbda70a5ff0e5b780599f6
RafiTahmid <rafi.tahmid019@gmail.com>
2022-09-25 15:04:43 +0600

    Generated data loaded:

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import { add } from '../../utilities/calculate';
+import React, { useEffect, useState } from 'react';
+import Cosmetic from '../Cosmetic/Cosmetic';
+// import { add } from '../../utilities/calculate';
 // import add from '../../utilities/calculate';
 
-const Cosmetics = () => {
-    const first = 50;
-    const second = 30;
-    const total = add(first , second)
+// const Cosmetics = () => {
+    // const first = 50;
+    // const second = 30;
+    // const total = add(first , second)
+
+    const Cosmetics = () =>{
+        const [cosmetics, setCosmetics] = useState([])
+
+        useEffect( () =>{
+            fetch('data.json')
+            .then(res => res.json())
+            .then(data => setCosmetics(data))
+        }, [])
     return (
         <div>
             <h1>Welcome to my Cosmetics Store</h1>
-            <h3>Total: {total}</h3>
+            {/* <h3>Total: {total}</h3> */}
+            {
+                cosmetics.map(cosmetic => <Cosmetic key={cosmetic.id} cosmetic={cosmetic}></Cosmetic>)
+            }
         </div>
     );
 };
 
-export default Cosmetics;
+export default Cosmetics
